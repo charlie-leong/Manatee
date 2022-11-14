fb9a59ac079f7fe78e2db90b83d1051dc7b19cd7
Areesha <areesha.durrani@hotmail.com>
2022-11-14 14:36:46 +0000

    Basic outline of request lesson page

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,13 @@
+
+from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
+class Request(models.Model):
+    availability =models.TextField()
+    numLessons=models.TextField()
+    interval = models.TextField()
+    duration=models.TextField()
+    extra =models.TextField()
+
+    
--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .forms import RequestForm
 
-# Create your views here.
+
+def request_lessons(request):
+    form = RequestForm()
+    return render(request, 'request-lessons.html', {'form': form})
+
+def home(request):
+    return render(request, 'home.html')
