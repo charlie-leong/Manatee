23facdc03c4110a451eec14d938dd3e82915e0f8
Areesha <areesha.durrani@hotmail.com>
2022-11-14 15:20:52 +0000

    new file

---
+++ lessons/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.core.validators import RegexValidator
+from .models import Request
+# Create your forms here.
+
+class RequestForm(forms.ModelForm):
+    class Meta:
+        model = Request
+        fields=['availability', 'numLessons', 'interval', 'duration', 'extra']
+        widgets = {'numLessons':forms.NumberInput()}
+
+    def clean(self):
+        pass
