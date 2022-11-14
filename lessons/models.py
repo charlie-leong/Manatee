6605bfe20e56f484f270eb90c9c4fe54da95e7f2
Areesha <areesha.durrani@hotmail.com>
2022-11-14 15:16:12 +0000

    Change request model fields

--- lessons/models.py
+++ lessons/models.py
@@ -1,13 +1,12 @@
-
 from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
 class Request(models.Model):
-    availability =models.TextField()
-    numLessons=models.TextField()
-    interval = models.TextField()
-    duration=models.TextField()
-    extra =models.TextField()
+    availability =models.CharField(max_length=10)
+    numLessons=models.CharField(max_length=10)
+    interval = models.CharField(max_length=10)
+    duration=models.CharField(max_length=10)
+    extra =models.CharField(max_length=10)
 
     
