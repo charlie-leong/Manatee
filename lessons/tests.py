7da8d8d06138739069f0af69038c22cef2cdddc0
sdharren <sdharren@yahoo.co.uk>
2022-11-17 17:30:53 +0000

    added setup and valid/invalid for user test case

--- lessons/tests.py
+++ lessons/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from .models import User
 
 # Create your tests here.
+class UserModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            "@johndoe",
+            first_name = "John",
+            last_name = "Doe",
+            email = "johndoe@example.org",
+            password = "Password123"
+        )
+    
+    def _asser_user_is_valid(self):
+        try:
+            self.user.full_clean()
+        except (ValidationError):
+            self.fail("Test user should be valid")
+    
+    def _assert_user_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.user.full_clean()
