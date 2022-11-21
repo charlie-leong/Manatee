8ba75f703840b38710f6ae28809ec55fa67f0911
zard01 <areesha.durrani@hotmail.com>
2022-11-21 22:45:16 +0000

    Connect Request model to database and create admin superuser to acces DB

--- lessons/views.py
+++ lessons/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .forms import RequestForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def request_lessons(request):
-    form = RequestForm()
+    if request.method == 'POST':
+        form = RequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('home'))
+
+    form = RequestForm(request.POST or None)
     return render(request, 'request-lessons.html', {'form': form})
 
 def home(request):
