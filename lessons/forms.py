77cb9bf3b5c123c57a22e99ef5df6beeece77bd9
Areesha <areesha.durrani@hotmail.com>
2022-11-22 15:17:09 +0000

    Remove widgets from forms.py and add default value to num_of_lessons field

--- lessons/forms.py
+++ lessons/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from django.core.validators import RegexValidator
+
 from .models import Request
 # Create your forms here.
 
 class RequestForm(forms.ModelForm):
     class Meta:
         model = Request
-        fields=['availability', 'numLessons', 'interval', 'duration', 'extra']
-        widgets = {'numLessons':forms.NumberInput(),'interval':forms.NumberInput(),'duration':forms.NumberInput()}
+        fields=['availability', 'number_of_lessons', 'interval', 'duration', 'extra']
+        #widgets = {'number_of_lessons':forms.NumberInput(),'interval':forms.NumberInput(),'duration':forms.NumberInput()}
 
     def clean(self):
         pass
--- lessons/models.py
+++ lessons/models.py
@@ -1,4 +1,5 @@
 from django.core.validators import RegexValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
@@ -12,7 +13,7 @@ AVAILABILITY = (
 
 class Request(models.Model):
     availability =models.CharField(max_length=10, choices=AVAILABILITY, default='monday')
-    numLessons=models.PositiveIntegerField()
+    number_of_lessons=models.PositiveIntegerField(default=1)
     interval = models.PositiveIntegerField()
     duration=models.PositiveIntegerField()
     extra =models.CharField(max_length=10, blank=True)
