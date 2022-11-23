87b88c463d6c3bafa43afeb950f93654f00c8896
sdharren <sdharren@yahoo.co.uk>
2022-11-23 17:01:31 +0000

    added admin model for user

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import *
+from .models import User, Request
 # Register your models here.
 
 
 admin.site.register(Request)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "username", "first_name", "last_name", "email", "is_staff", "is_superuser"
+    ]
