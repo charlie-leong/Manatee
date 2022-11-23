3706291f0ec122fbd0726d40d05d0d0324bad710
sdharren <sdharren@yahoo.co.uk>
2022-11-23 17:03:26 +0000

    added admin model for requests

--- lessons/admin.py
+++ lessons/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from .models import User, Request
 # Register your models here.
 
-
-admin.site.register(Request)
-
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = [
         "username", "first_name", "last_name", "email", "is_staff", "is_superuser"
     ]
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    list_display = {
+        "availability", "number_of_lessons", "interval", "duration", "extra"
+    }
