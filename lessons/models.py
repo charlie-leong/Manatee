a5c2036e07561f5b6969565a96948076bcb7cccf
Charlotte Leong <charlotte.leong@kcl.ac.uk>
2022-11-26 16:42:13 +0000

    limit lesson duration options in Request model

--- lessons/models.py
+++ lessons/models.py
@@ -12,11 +12,18 @@ AVAILABILITY = (
     ('friday','FRIDAY'),
 )
 
+DURATION_OPTIONS = (
+    (30, '30'),
+    (45, '45'),
+    (60, '60'),
+    (90, '90')
+)
+
 class Request(models.Model):
     availability =models.CharField(max_length=10, choices=AVAILABILITY, default='monday')
     number_of_lessons=models.PositiveIntegerField(default=1)
     interval = models.PositiveIntegerField()
-    duration=models.PositiveIntegerField()
+    duration=models.PositiveIntegerField(choices=DURATION_OPTIONS, default=30)
     extra =models.CharField(max_length=10, blank=True)
 
 class User(AbstractUser):
@@ -32,8 +39,8 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, blank=False)
    
 class Lesson(models.Model):
-    assigned_student_id = models.CharField()
-    assigned_teacher_id = models.CharField()
+    assigned_student_id = models.CharField(max_length = 10)
+    assigned_teacher_id = models.CharField(max_length = 10)
     number_of_lessons = models.PositiveIntegerField()
     week_interval = models.PositiveIntegerField()
     duration = models.PositiveIntegerField()
@@ -46,15 +53,3 @@ class Lesson(models.Model):
         paid = True
         return paid
 
-    def calculateCost():
-        baseCost = 20
-        #change to be adjusted based on duration of lesson
-        totalCost = baseCost * duration/ 60 * number_of_lessons
-        return totalCost
-
-    def setAvailability(mon, tue, wed, thu, fri):
-        monAvailability = mon
-        tueAvailability = tue
-        wedAvailability = wed
-        thuAvailability = thu
-        friAvailability = fri
