f6509060dbd7ac603ac06e4f6f0c0eefc22e7c36
g-80 <ghaith.ger@gmail.com>
2022-11-29 18:54:55 +0000

    Revert "Merge branch 'main' into request-lessons"
    This reverts commit 6293ed7f6aff6eae61ea8b030adc54d545189117, reversing
    changes made to 12342d0a2eddbc7b3c6c1aa7596eb2e0d7aa25b1.

--- lessons/admin.py
+++ lessons/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "username", "first_name", "last_name", "email", "is_staff", "is_superuser"
+    ]
+
 @admin.register(Request)
 class RequestAdmin(admin.ModelAdmin):
     list_display = [
         "availability", "number_of_lessons", "interval", "duration", "extra_info", "created_by", "is_approved"
     ]
-
