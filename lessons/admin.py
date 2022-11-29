3e75f7058c00d290d03e5d3bfc3bf1bbd9b4a388
g-80 <ghaith.ger@gmail.com>
2022-11-29 15:44:11 +0000

    fix Request model admin fields

--- lessons/admin.py
+++ lessons/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    list_display = [
+        "availability", "number_of_lessons", "interval", "duration", "extra_info", "created_by", "is_approved"
+    ]
 
-admin.site.register(Request)
