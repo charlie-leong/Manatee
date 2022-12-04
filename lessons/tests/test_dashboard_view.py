f03c9ae28e3262fe4df17c6e48b85f7b321076c3
sdharren <sdharren@yahoo.co.uk>
2022-12-04 17:27:47 +0000

    added test cases for dashboard view
    added test cases for dashboard view.
    added a new helper function to construct redirect url query

---
+++ lessons/tests/test_dashboard_view.py
@@ -0,0 +1,34 @@
+from re import T
+from django.test import TestCase
+from django.urls import reverse
+from .helpers import reverse_with_next
+from lessons.models import User
+
+class DashboardViewTestCase(TestCase):
+
+    def setUp(self):
+        self.url = reverse("dashboard")
+        self.user = User.objects.create_user(
+            "@johndoe",
+            first_name = "John",
+            last_name = "Doe",
+            email = "johndoe@example.org",
+            password = "Password123"
+        )
+
+    # test that the url is correct
+    def test_dashboard_view_has_correct_url(self):
+        self.assertEqual(self.url, "/dashboard/")
+
+    # test when a user is not logged in, they are redirected to the log in page
+    def test_not_logged_in_user_redirects_to_log_in_page(self):
+        redirect_url = reverse_with_next("log_in", self.url)
+        response = self.client.get(self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    # test get dashboard view
+    def tests_get_dashboard_view(self):
+        self.client.login(username=self.user.username, password=self.user.password)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "dashboard.html")
