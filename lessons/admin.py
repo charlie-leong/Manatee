cff9bab8432484009676ca03e4d4d668dd0f506c
sdharren <sdharren@yahoo.co.uk>
2022-12-08 00:48:28 +0000

    request admin tables are split into pending and fulfilled

--- lessons/admin.py
+++ lessons/admin.py
@@ -11,8 +11,30 @@ class UserAdmin(admin.ModelAdmin):
 class LessonInline(admin.TabularInline):
     model = Lesson
 
-@admin.register(Request)
-class RequestAdmin(admin.ModelAdmin):
+class pendingRequest(Request):
+    class Meta:
+        proxy = True
+        verbose_name = "Pending Request"
+
+class approvedRequest(Request):
+    class Meta:
+        proxy = True
+        verbose_name = "Fulfilled Request"
+
+@admin.register(pendingRequest)
+class pendingRequestAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return Request.objects.filter(is_approved = False)
+    list_display = [
+       "id", "user", "availability", "number_of_lessons", "interval", "duration", "is_approved"
+    ]
+    readonly_fields = ["is_approved"]
+    inlines = [LessonInline]
+        
+@admin.register(approvedRequest)
+class approvedRequestAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return Request.objects.filter(is_approved = True)
     list_display = [
        "id", "user", "availability", "number_of_lessons", "interval", "duration", "is_approved"
     ]
